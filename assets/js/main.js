5d8614c18f5bb18e8234c12ef597d4a453582fbc
matiusfebrianchristo <chriss.tkj@gmail.com>
2025-01-23 03:13:35 +0700

    update 23/1/2025

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ const name_person = document.querySelector('input[name="name"]')
 const email_add = document.querySelector('input[name="email"]')
 const subject_mail = document.querySelector('input[name="subject"]')
 const message = document.querySelector('input[name="message"]')
+let numberCardCertificate = 1
 
 
 window.addEventListener('scroll', () => {
@@ -69,18 +70,109 @@ mobile_navbar.addEventListener('click', e => {
 
 
 
-function sendMail(){
-  if (!name_person.value || !email_add.value || !subject_mail.value || !message.value) {
-    console.log("fill blank input")
-  } else {
-    console.log(name_person.value)
-    console.log(email_add.value)
-    console.log(subject_mail.value)
-    console.log(message.value)
+// function sendMail(){
+//   if (!name_person.value || !email_add.value || !subject_mail.value || !message.value) {
+//     console.log("fill blank input")
+//   } else {
+//     // console.log(name_person.value)
+//     // console.log(email_add.value)
+//     // console.log(subject_mail.value)
+//     // console.log(message.value)
+//     let link = `mailto:matiusfebrianchristo@gmail.com?from=${encodeURIComponent(email_add.value)}&subject=${encodeURIComponent(subject_mail.value)+" | " +encodeURIComponent(name_person.value)}&body=${encodeURIComponent(message.value)}`
+//     window.location.href = link
     
-    // let link = `mailto:matiusfebrianchristo@gmail.com?from=${encodeURIComponent(email_add.value)}&subject=${encodeURIComponent(subject_mail.value)+" | " +encodeURIComponent(name_person.value)}&body=${encodeURIComponent(message.value)}`
-    // window.location.href = link
-    
+//   }
+
+// }
+function counterTimeWork(){
+  const today = new Date()
+  const JoinDay = new Date("January 2, 2023")
+  const msPerDay = 24 * 60 * 60 * 1000
+  const countTime = (today.getTime() - JoinDay.getTime())
+  const e_countDays = countTime / msPerDay;
+  let countDays = Math.floor(e_countDays);
+  let countYears = 0
+  let countMonths = 0
+
+  if (countDays > 365) {
+    countYears = Math.floor(countDays / 365);
+    countDays = countDays % 365;
+  }
+  if (countDays > 30) {
+    countMonths = Math.floor(countDays / 30)
+    countDays = countDays % 30;
   }
 
+  // const e_hrsLeft = (e_countDays - countDays) * 24;
+  // const hrsLeft = Math.floor(e_hrsLeft);
+  // const minsLeft = Math.floor((e_hrsLeft - hrsLeft) * 60);
+  if (countMonths > 0) {
+    return `${countYears} years and ${countMonths} months`
+  } else {
+    return `${countYears} years`
+  }
+
+}
+
+const tag_counterWorking = document.querySelector('.counterWorking')
+tag_counterWorking.innerHTML= counterTimeWork()
+
+function changeSlide(isNext) {
+  let moved = 0
+  const mediaQuery768  = window.matchMedia('(max-width: 768px)')
+  if (mediaQuery768.matches) {
+    moved = -27
+  } else {
+    moved = -56
+  }
+  
+  if(isNext) {
+    if (numberCardCertificate <6) {
+      document.querySelectorAll('#certificate .card-certificate').forEach(e => {
+        e.style.transform = `translateX(${moved*numberCardCertificate}rem)`
+      })
+      numberCardCertificate += 1
+    }
+
+  } else if (!isNext) {
+    if (numberCardCertificate > 1) {
+      if (numberCardCertificate == 2) {
+        numberCardCertificate -= 1
+        document.querySelectorAll('#certificate .card-certificate').forEach(e => {
+          e.style.transform = `translateX(0)`
+        })
+      } else {
+        numberCardCertificate -=1
+        let minusNumber = numberCardCertificate - 1
+        document.querySelectorAll('#certificate .card-certificate').forEach(e => {
+          e.style.transform = `translateX(${moved*minusNumber}rem)`
+        })
+      }
+    }
+  }
+  
+  // console.log(card)
+}
+
+function openLink(link){
+  window.open(
+    link,
+    '_blank' // <- This is what makes it open in a new window.
+  );
+}
+
+function viewCertificate(link) {
+  if (link == '1OP81Y44LZQK') {
+    openLink('https://www.dicoding.com/certificates/1OP81Y44LZQK')
+  } else if (link == 'QLZ949D4MP5D') {
+    openLink('https://www.dicoding.com/certificates/QLZ949D4MP5D')
+  } else if (link == 'L4PQGNVE2ZO1') {
+    openLink('https://www.dicoding.com/certificates/L4PQGNVE2ZO1')
+  } else if (link == '287RYZ5MSKKH') {
+    openLink('https://www.coursera.org/account/accomplishments/specialization/certificate/287RYZ5MSKKH')
+  } else if (link == 'AC-097-37056374-98') {
+    openLink('https://apidev.agunacourse.com/media/media/certificate/AC-097-37056374-98.pdf')
+  } else if (link == 'nokia-nrs-1') {
+    openLink('https://www.credly.com/badges/c03a62e8-fc30-4e84-a01e-dc55f95945d7/linked_in_profile')
+  }
 }
